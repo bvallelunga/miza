7a0bf13672bec34a12bd1618bf142eaf41561f87
Brian Vallelunga <vallelungabrian@gmail.com>
2016-08-25 00:17:36 -0700

    fixed v1 script

--- servers/ads/views/v1/script.js
+++ servers/ads/views/v1/script.js
@@ -1,22 +1,21 @@
 (function(window) {
-  var API = window["<%= publisher.key %>"] = {}
+  var API = {}
   
   <% networks.forEach(function(network) { %>
     window["<%= publisher.key %>_<%= network.id %>"] = <%= network.entry_js %>
   <% }) %>
   
-  API.init = function() {
-    API.id = "<%= publisher.key %>" 
-    API.base = "//<%= publisher.endpoint %>/" 
-    API.prod = <%= CONFIG.is_prod %>
-    API.window = window
-    API.head = document.head
-    API.networks = []
-    API.targets = {}
-    API.targets_keys = []
-    API.natives = {}
-    API.attribute_params = ""
-    API.attributes = {
+  API.s_init = function() {
+    API.s_id = "<%= publisher.key %>" 
+    API.s_base = "//<%= publisher.endpoint %>/" 
+    API.s_prod = <%= CONFIG.isProd %>
+    API.s_window = window
+    API.s_head = document.getElementsByTagName('head')[0]
+    API.s_targets = {}
+    API.s_targets_list = []
+    API.s_natives = {}
+    API.s_attribute_params = ""
+    API.s_attributes = {
       "battery": {},
       "demensions": {},
       "plugins": [],
@@ -29,37 +28,36 @@
     
     <% networks.forEach(function(network) { %>
       var target = new RegExp("<%= network.targets %>")
-      API.targets[target] = "<%= network.id %>"
-      API.targets_keys.push(target)
-      API.networks.push("<%= network.id %>")
+      API.s_targets[target] = "<%= network.id %>"
+      API.s_targets_list.push(target)
     <% }) %>
     
     <% if(enabled) { %> 
-      API.listeners(window.document)
-      API.overrides(window)
+      API.s_listeners(window.document)
+      API.s_overrides(window)
     <% } %>
     
-    API.fetch_attributes(window, API.start)
+    API.s_fetch_attributes(window, API.s_start)
   }
   
-  API.start = function() {
+  API.s_start = function() {
     <% networks.forEach(function(network) { %> 
-      var script = API.script()
+      var script = API.s_script()
       
       <% if(enabled) { %>
-        script.src = API.url("<%= network.entry_url %>", false, "<%= network.id %>")
+        script.src = API.s_url("<%= network.entry_url %>", false, "<%= network.id %>")
       <% } else { %>
         script.src = "<%= network.entry_raw_url %>"
       <% } %>
       
-      API.head.appendChild(script)
+      API.s_head.appendChild(script)
     <% }) %>
     
-    API.status("p")
+    API.s_status("p")
   }
   
-  API.overrides = function(w) {    
-    Array(
+  API.s_overrides = function(w) {    
+    [
       [w.Element.prototype, "appendChild"],
       [w.Element.prototype, "insertBefore"],
       [w.Document.prototype, "appendChild"],
@@ -70,23 +68,23 @@
       [w.document, "insertBefore"],
       [w.document, "writeln"],
       [w.document, "write"]
-    ).forEach(function(override) {
-      var method = API.natives[override[1]] = API.natives[override[1]] || override[0][override[1]]
-      override[0][override[1]] = API.method_override(w, method)
+    ].forEach(function(override) {
+      var method = API.s_natives[override[1]] = API.s_natives[override[1]] || override[0][override[1]]
+      override[0][override[1]] = API.s_method_override(w, method)
     }) 
   }
   
-  API.method_override = function(window, method) {
-    return function(content) {      
+  API.s_method_override = function(window, method) {
+    return function(content) {
       if(["writeln", "write"].indexOf(method.name) > -1) {
-        return API.override_writeln(window, this, content, method) 
+        return API.s_override_writeln(window, this, content, method) 
       }
       
       for(var i = 0; i < arguments.length; i++) {
         node = arguments[i]
         
         if(node instanceof Element) {
-          arguments[i] = API.migrator(window.document, node, false)
+          arguments[i] = API.s_migrator(node, false)
         }
       }
     
@@ -94,36 +92,18 @@
     }
   }
   
-  API.override_writeln = function(window, document, content, method) {  
-    content = content.replace(/(?:src|href)=["|']([^"]*)["|']/gi, function(match, url) {
-      var network = API.fetch_target(url)
-      
-      if(!network) return match
-      
-      return match.replace(url, API.url(url, true, network))
-    })
-    
+  API.s_override_writeln = function(window, document, content, method) {  
     if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
       return method.apply(document, [content])
     }
-    
-    if(method.name == "write") {
-      API.natives.write.apply(document, [""])
-      API.listeners(document)
-      API.overrides(window)
-    }
       
-    var out = API.cleaner(document, content)
-    API.natives.writeln.apply(document, [out[0]])
-    document.head.appendChild(out[1])
-    
-    API.children(document).forEach(function(element) {
-      API.migrator(document, element)
-    })
+    var out = API.s_cleaner(document, content)
+    document.body.innerHTML += out[0]
+    document.getElementsByTagName('head')[0].appendChild(out[1])
   } 
   
-  API.fetch_attributes = function(window, callback) {
-    var attributes = Object.keys(API.attributes)
+  API.s_fetch_attributes = function(window, callback) {
+    var attributes = Object.keys(API.s_attributes)
      
     attributes.forEach(function(key, i) {
       switch(key) {        
@@ -131,7 +111,7 @@
           if(!window.navigator.getBattery) break
           
           window.navigator.getBattery().then(function(battery) {
-            API.attributes[key] = {
+            API.s_attributes[key] = {
               charging: battery.charging,
               charging_time: battery.chargingTime,
               level: battery.level
@@ -141,20 +121,20 @@
           break
           
         case "demensions":
-          API.attributes[key] = {
+          API.s_attributes[key] = {
             width: window.innerWidth,
             height: window.innerHeight
           }
           break
       
         case "protected":
-          API.blocker_check(window, function(value) {
-            API.attributes[key] = value
+          API.s_blocker_check(window, function(value) {
+            API.s_attributes[key] = value
           })
           break
           
         case "plugins":
-          API.attributes[key] = Object.keys(window.navigator.plugins || []).map(function(id) {  
+          API.s_attributes[key] = Object.keys(window.navigator.plugins || []).map(function(id) {  
             var plugin = navigator.plugins[id]
             
             return {
@@ -166,14 +146,14 @@
           break
           
         case "languages":
-          API.attributes[key] = window.navigator.languages || []
+          API.s_attributes[key] = window.navigator.languages || []
           break
           
         case "components":
           if(!window.navigator.mediaDevices) break
         
           window.navigator.mediaDevices.enumerateDevices().then(function(devices) {
-            API.attributes[key] = devices.map(function(device) {
+            API.s_attributes[key] = devices.map(function(device) {
               return {
                 device_id: device.deviceId,
                 group_id: device.groupId,
@@ -185,58 +165,58 @@
           break
           
         case "do_not_track":
-          API.attributes[key] = window.navigator.doNotTrack == "1"
+          API.s_attributes[key] = window.navigator.doNotTrack == "1"
           break
       }
       
       if(i == attributes.length-1) {
         setTimeout(function() {
-          API.attribute_params = API.serialize(API.attributes)
+          API.s_attribute_params = API.s_serialize(API.s_attributes)
           callback()
         }, 500)
       } 
     })
   }
   
-  API.serialize = function(obj, prefix) {
+  API.s_serialize = function(obj, prefix) {
     var str = [];
     for(var p in obj) {
       if (obj.hasOwnProperty(p)) {
         var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
         str.push(typeof v == "object" ?
-          API.serialize(v, k) :
+          API.s_serialize(v, k) :
           encodeURIComponent(k) + "=" + encodeURIComponent(v));
       }
     }
     return str.join("&");
   }
     
-  API.listeners = function(document) {
-    if(!document) return
+  API.s_listeners = function(element) {
+    if(!element) return
     
-    document.addEventListener('DOMNodeInserted', function(event) {
-      API.migrator(document, event.target) 
+    element.addEventListener('DOMNodeInserted', function(event) {
+      API.s_migrator(event.target) 
     })
   }
   
-  API.url = function(url, encode, network) {    
+  API.s_url = function(url, encode, network) {
     var encoded = (encode != false) ? btoa(url) : url
   
     return (
-      API.base + encoded + "?" + API.attribute_params +
-      (network ? ("&network=" + network) : "") + "&"
+      API.s_base + encoded + "?" + API.s_attribute_params +
+      (network ? ("&network=" + network) : "")
     )
   }
   
-  API.script = function() {
+  API.s_script = function() {
     var script = document.createElement('script')
     script.async = true
     script.setAttribute('type', 'text/javascript')
     return script
   }
 	  
-  API.cleaner = function(document, text) {         
-    var script = API.script()
+  API.s_cleaner = function(document, text) {         
+    var script = API.s_script()
     var cleaned = text.replace(/<script(.*?)>([\s\S]*?)<\/script>/gi, function() {
       if(arguments[1].indexOf("src") > -1) 
         return arguments[0]
@@ -248,7 +228,7 @@
     return [cleaned, script]
   }
   
-  API.blocker_check = function(window, callback) {
+  API.s_blocker_check = function(window, callback) {
     var test = document.createElement('div')
     test.innerHTML = '&nbsp;'
     test.className = 'adsbox'
@@ -260,9 +240,9 @@
     }, 100)
   }
   
-  API.status = function(status, network) {
+  API.s_status = function(status, network) {
     var img = document.createElement('img')
-    img.src = API.url(status, false, network)
+    img.src = API.s_url(status, false, network)
     img.style.display = "none"
     document.body.appendChild(img)
     img.onload = function() {
@@ -270,66 +250,48 @@
     }
   }
   
-  API.fetch_target = function(src) {  
-    if(!src) return null
-      
-    for(var i = 0; i < API.targets_keys.length; i++) {
-      var tester = API.targets_keys[i]
+  API.s_fetch_target = function(src) {    
+    for(var i = 0; i < API.s_targets_list.length; i++) {
+      var tester = API.s_targets_list[i]
       
       if(tester.test(src)) {
-        return API.targets[tester]
+        return API.s_targets[tester]
       }
     }
     
     return null
   }
   
-  API.children = function(element) {
-    return Array().slice.call(element.getElementsByTagName("*"))
-  } 
-  
-  API.migrator = function(document, element, to_replace) {     
+  API.s_migrator = function(element, to_replace) {     
     var path = element.src ? "src" : "href"
     var src = element.src || element.href
     var tagName = (element.tagName || "").toLowerCase()
-    var network = API.fetch_target(src)
+    var network = API.s_fetch_target(src)
     
     if (network != null) {      
       var orginal = element
-      element = (tagName == "script") ? API.script() : element
+      element = (tagName == "script") ? API.s_script() : element
       element.async = orginal.async
       element.type = orginal.type
-      element[path] = API.url(src, true, network)
+      element[path] = API.s_url(src, true, network)
       
       if(tagName == "a") {
         element[path] += "&link=true"
-        API.status("i", network)
+        API.s_status("i", network)
       }
       
-      if(to_replace != false) {
-        if(orginal.parentNode) {
-          orginal.parentNode.replaceChild(element, orginal)
-        } else {
-          API.natives.appendChild.apply(document.body, [element])
-          
-          if(tagName == "script") {
-            orginal.remove()
-          }
-        } 
-      }
+      if(to_replace != false)
+        orginal.parentNode.replaceChild(element, orginal)
     }
     
-    if(tagName == "iframe" && element.contentDocument != null) {  
-      API.listeners(element.contentDocument)
-      API.overrides(element.contentWindow)
+    if(tagName == "iframe" && element.contentDocument != null) {    
+      API.s_listeners(element.contentDocument)
+      API.s_overrides(element.contentWindow)
     }
-    
-    API.children(element).forEach(function(element) {
-      API.migrator(document, element)
-    })
-    
+        
+    [].slice.call(element.childNodes).forEach(API.s_migrator)
     return element
   }
   
-  API.init()
+  API.s_init()
 })(window)
