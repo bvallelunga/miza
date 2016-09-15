647d06b572a6a6b3398fbe7b412a1769988bd524
Brian Vallelunga <vallelungabrian@gmail.com>
2016-09-15 11:43:20 -0700

    added scripts folder

---
+++ scripts/prod_deploy.sh
@@ -0,0 +1,7 @@
+BRANCH=git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/';
+
+bash ./npm_prepare.sh;
+git checkout master;
+git merge $BRANCH;
+git push origin master;
+git checkout $BRANCH;
