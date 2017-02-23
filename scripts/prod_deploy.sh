70d444657008ce1f6a5e3435417cd41975a5aab9
Brian Vallelunga <vallelungabrian@gmail.com>
2017-02-23 00:13:37 -0800

    disabled prod-deploy

--- scripts/prod_deploy.sh
+++ scripts/prod_deploy.sh
@@ -4,8 +4,9 @@ ROOT=$(git rev-parse --show-toplevel)
 #bash $ROOT/scripts/npm_prepare.sh;
 #git add $ROOT/npm-shrinkwrap.json $ROOT/package.json;
 #git commit -m "updated npm-shrinkwrap.json";
-git push origin $BRANCH;
-git checkout master;
-git merge $BRANCH;
-git push origin master;
-git checkout $BRANCH;
+
+# git push origin $BRANCH;
+# git checkout master;
+# git merge $BRANCH;
+# git push origin master;
+# git checkout $BRANCH;
