f551078c5d0583584783cd5a661e38b47871807d
Brian Vallelunga <vallelungabrian@gmail.com>
2017-03-24 12:25:30 -0700

    updated prod deploy

--- scripts/prod_deploy.sh
+++ scripts/prod_deploy.sh
@@ -1,12 +1,12 @@
 BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/');
 ROOT=$(git rev-parse --show-toplevel)
 
-#bash $ROOT/scripts/npm_prepare.sh;
-#git add $ROOT/npm-shrinkwrap.json $ROOT/package.json;
-#git commit -m "updated npm-shrinkwrap.json";
+# bash $ROOT/scripts/npm_prepare.sh;
+# git add $ROOT/npm-shrinkwrap.json $ROOT/package.json;
+# git commit -m "updated npm-shrinkwrap.json";
 
-# git push origin $BRANCH;
-# git checkout master;
-# git merge $BRANCH;
-# git push origin master;
-# git checkout $BRANCH;
+git push origin $BRANCH;
+git checkout master;
+git merge $BRANCH;
+git push origin master;
+git checkout $BRANCH;
